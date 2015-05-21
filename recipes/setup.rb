362f49205a445b7b0a7786d5b9263441b955cc0f
root <root@ip-172-31-57-13.ec2.internal>
2015-05-21 20:12:43 +0000

    motd changes

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -3,5 +3,10 @@ action :install
 end
 
 file "/etc/motd" do
-  content "Property of Chefco"
+  content "Property of Chefco
+  IPADDRESS: #{node["ipaddress"]}
+  HOSTNAME : #{node["hostname"]}
+  MEMORY   : #{node["memory"]["total"]}
+  CPU      : #{node["cpu"]["0"]["mhz"]}
+  "
 end
